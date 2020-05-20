287c7d61a419aace6464f3b88101c5a04b687659
Eren Sırakaya <poisonweeb@gmail.com>
2020-05-20 21:49:01 +0300

    data memory & alu control added

--- mips_processor.v
+++ mips_processor.v
@@ -1,48 +1,155 @@
 
 module control (input [5:0] instruction,
-					output reg_dst, jump, branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write);
+					output reg_dst, jump, branch, mem_read, mem_to_reg, [2:0] alu_op, mem_write, alu_src, reg_write);
 
 	/*
 	instruction    reg_dst    jump    branch    mem_read     mem_to_reg    alu_op    mem_write    alu_src    reg_write		
-	  000000          1        0        0         0             0            1          0           0           1	
-	  000001          0        0        1         0             0            1          0           0           0	
-	  000010          0        1        0         0             0            0          0           0           0	
-	  000011          1        1        0         0             1            0          0           0           1	
-	  000100          0        0        1         0             0            1          0           0           0	
-	  000101          0        0        1         0             0            1          0           0           0	
-	  000110          0        0        1         0             0            1          0           0           0	
-	  001001          0        0        0         0             0            1          0           1           1	
-	  001010          0        0        0         0             0            1          0           1           1
-	  001011          0        0        0         0             0            1          0           1           1
-	  001100          0        0        0         0             0            1          0           1           1	
-	  001101          0        0        0         0             0            1          0           1           1	
-	  001110          0        0        0         0             0            1          0           1           1          	
-	  001111		  0		   0        0		  0             0            1          0           1           1
-	  100000		  0	       0        0         1             1          	 0          0           1           1
-	  100011		  0	       0        0         1             1          	 0          0           1           1 	
-	  101000		  0        0        0         0             0            0          1           1           0          	
-	  101011		  0        0        0         0             0            0          1           1           0
+	  000000          1        0        0         0             0           111         0           0           1
+	  000010          0        1        0         0             0           000         0           0           0
+	  000011          0        1        0         0             1           000         0           0           1
+	  000100          0        0        1         0             0           001         0           0           0
+	  000101          0        0        1         0             0           101         0           0           0
+	  001000          0		   0		0		  0				0			000			0			1			1
+	  001001          0        0        0         0             0           000         0           1           1
+	  001010          0        0        0         0             0           001         0           1           1
+	  001011          0        0        0         0             0           001         0           1           1
+	  001100          0        0        0         0             0           010         0           1           1
+	  001101          0        0        0         0             0           011         0           1           1
+	  001110          0        0        0         0             0           100         0           1           1
+	  100000		  0	       0        0         1             1          	000         0           1           1
+	  100011		  0	       0        0         1             1          	000         0           1           1
+	  101000		  0        0        0         0             0          	000         1           1           0
+	  101011		  0        0        0         0             0           000         1           1           0
 	*/
 	
-	assign reg_dst 		= (instruction[5:2] == 4'b0) ? 
-							(((instruction[1:0] == 2'b00) || (instruction[1:0] == 2'b11)) ? 1'b1 : 1'b0) : 1'b0;
-	assign jump 		= (instruction[5:2] == 4'b0) ? 
-							(((instruction[1:0] == 2'b10) || (instruction[1:0] == 2'b11)) ? 1'b1 : 1'b0) : 1'b0;
-	assign branch 		= (instruction[5:2] == 4'b0001) ? 1'b1 : 
-							((instruction == 5'b000001) ? 1'b1 : 1'b0);
-	assign mem_read		= ({instruction[5], instruction[3]} == 2'b10) ? 1'b1 : 1'b0;
+	reg [10:0] out;
 	
-	assign mem_to_reg 	= ({instruction[5], instruction[3]} == 2'b10) ? 1'b1 : 
-							((instruction == 5'b000011) ? 1'b1 : 1'b0);
-	assign alu_op 		= (instruction[5] == 1'b1) ? 1'b0 : 
-							((instruction == 5'b000010 || instruction == 5'b000011) ? 1'b0 : 1'b1);
-	assign mem_write 	= (instruction[5:3] == 3'b101) ? 1'b1 : 1'b0;
-	
-	assign alu_src		= (instruction[5:3] == 3'b0) ? 1'b0 : 1'b1;
-	
-	assign reg_write	= ((instruction[5:2] == 4'b0001) || (instruction[5:3] == 3'b101)) ? 1'b0 :
-							(((instruction == 5'b000001) || (instruction == 5'b000010)) ? 1'b0 : 1'b1);
+	always @ (*) begin
+		case (instruction)
+			6'b000000:
+				out = 11'b10000111001;
+			6'b000010:
+				out = 11'b01000000000;
+			6'b000011:
+				out = 11'b01001000001;
+			6'b000100:
+				out = 11'b00100001000;
+			6'b000101:
+				out = 11'b00100101000;
+			6'b001000:
+				out = 11'b00000000011;
+			6'b001001:
+				out = 11'b00000000011;
+			6'b001010:
+				out = 11'b00000001011;
+			6'b001011:
+				out = 11'b00000001011;
+			6'b001100:
+				out = 11'b00000010011;
+			6'b001101:
+				out = 11'b00000011011;
+			6'b001110:
+				out = 11'b00000100011;
+			6'b100000:
+				out = 11'b00011000011;
+			6'b100011:
+				out = 11'b00011000011;
+			6'b101000:
+				out = 11'b00000000110;
+			6'b101011:
+				out = 11'b00000000110;
+			default:
+				out = 11'b0;
+		endcase
+	end
 
+	assign reg_dst 		= out[10];
+	assign jump 		= out[9];
+	assign branch 		= out[8];
+	assign mem_read 	= out[7];
+	assign mem_to_reg 	= out[6];
+	assign alu_op 		= out[5:3];
+	assign mem_write 	= out[2];
+	assign alu_src 		= out[1];
+	assign reg_write 	= out[0];
+
+endmodule
+
+module alu_control(input [2:0] alu_op, [5:0] func,
+					output [3:0] alu_code);
+					
+	reg [3:0] out;
+	
+	// addition 		4'b0000
+	// subtraction 		4'b0001
+	// multiplication 	4'b0010
+	// division			4'b0011
+	
+	// and				4'b0100
+	// or				4'b0101
+	// nor				4'b0110
+	// xor				4'b0111
+	
+	// shift_right		4'b1000
+	// shift_left		4'b1001
+	// sub_not			4'b1010
+					
+	always @ (*) begin
+		case (alu_op)
+			3'b000:
+				out = 4'b0000;
+			3'b001:
+				out = 4'b0001;
+			3'b010:
+				out = 4'b0100;
+			3'b011:
+				out = 4'b0101;
+			3'b100:
+				out = 4'b0111;
+			3'b101:
+				out = 4'b1010;
+			3'b111:
+				case (func)
+					5'b100000:		// addition
+						out = 4'b0000;
+					5'b100001:		// addition u
+						out = 4'b0000;
+					5'b100100:		// and
+						out = 4'b0100;
+					5'b100111:		// nor
+						out = 4'b0110;
+					5'b100101:		// or
+						out = 4'b0101;
+					5'b101010:		// slt
+						out = 4'b0001;
+					5'b101011:		// slt u
+						out = 4'b0001;
+					5'b100010:		// sub
+						out = 4'b0001;
+					5'b100011:		// sub u
+						out = 4'b0001;
+					5'b100110:		// xor
+						out = 4'b0111;
+					5'b000000:		// shift left
+						out = 4'b1001;
+					5'b000100:		// shift left
+						out = 4'b1001;
+					5'b000011:		// shift right
+						out = 4'b1000;
+					5'b000111:		// shift right
+						out = 4'b1000;
+					5'b000010:		// shift right
+						out = 4'b1000;
+					5'b000110:		// shift right
+						out = 4'b1000;
+				endcase
+			default:
+				out = 4'b0;
+		endcase
+	end
+	
+	assign alu_code = out;
+	
 endmodule
 
 module register (input clk, reg_write, [4:0] reg_1, [4:0] reg_2, [4:0] write_reg, [31:0] write_data, 
@@ -205,6 +312,28 @@ module multiplier_31_bit (input [31:0] x, [15:0] y,
 				res = res + t_x;
 			t_x = t_x << 1;
 		end
+	end
+
+endmodule
+
+module data_memory(input mem_read, mem_write, [31:0] address, [31:0] write_data, 
+					output reg [31:0] read_data);
+
+	reg [31:0] mem [31:0];
+	
+	integer i;
+	initial begin
+		for (i=0; i<32; i=i+1)
+			mem[i] = 32'b0;
+	end
+	
+	always @ (*) begin
+		if (mem_write)
+			mem[address] = write_data;
+		if (mem_read)
+			read_data = mem[address];
+		else
+			read_data = 32'b0;
 	end
 
 endmodule
